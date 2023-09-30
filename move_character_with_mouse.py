e377a29421e3bc4c574e9633188203a4cfd3f18c
Lee9747 <leejunho32@naver.com>
2023-09-30 17:17:03 +0900

    Add Character Direction

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -10,22 +10,25 @@ arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-    pass
+
 
 def random_hand_point():
     return random.randint(0, TUK_WIDTH), random.randint(0, TUK_HEIGHT)
 
 
 def move_character(point_x, point_y):
-    global x, y, arrow_x, arrow_y
+    global x, y, arrow_x, arrow_y, dir
 
+    if x < point_x:
+        dir = 1
+    else:
+        dir = -1
 
     t = 0.03
     x = (1 - t) * x + t * point_x
@@ -34,10 +37,10 @@ def move_character(point_x, point_y):
     dis = math.sqrt( (point_x - x) ** 2 + (point_y - y) ** 2 )
     if dis < 5:
         arrow_x, arrow_y = random_hand_point()
-    pass
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+dir = 1
 arrow_x, arrow_y = random_hand_point()
 frame = 0
 hide_cursor()
@@ -48,8 +51,11 @@ while running:
 
     move_character(arrow_x, arrow_y)
 
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     arrow.draw(arrow_x, arrow_y)
+    if dir == 1:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    elif dir == -1:
+        character.clip_composite_draw(frame * 100, 100 * 1, 100, 100, 0, 'h', x, y, 100, 100)
 
     update_canvas()
     frame = (frame + 1) % 8

6e7cad9100f61e9a5f5600dadf43ff0c0472b3b2
Lee9747 <leejunho32@naver.com>
2023-09-30 17:00:27 +0900

    Complete the frame

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,11 +1,12 @@
 from pico2d import *
+import random
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-
+arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
@@ -14,26 +15,36 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
+
+def random_hand_point():
+    pass
+
+def move_character(point_x, point_y):
+    pass
+
 
 
 
 running = True
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+arrow_x, arrow_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+
+    move_character(arrow_x, arrow_y)
+
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    arrow.draw(arrow_x, arrow_y)
+
     update_canvas()
     frame = (frame + 1) % 8
-
     handle_events()
 
 close_canvas()
